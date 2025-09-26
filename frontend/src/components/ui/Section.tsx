4eb6c5e3ba2904bf3dbeab96e9a67b6785cc8b04
Paul Deveaux <paul.deveaux1@gmail.com>
2025-09-26 11:21:47 +0200

    feat(ui): change remote to local images

--- frontend/src/components/ui/Section.tsx
+++ frontend/src/components/ui/Section.tsx
@@ -5,6 +5,7 @@ import React from "react";
 
 
 export interface SectionProps {
+    id?: string;
     children?: React.ReactNode;
     bgColor?: string;
     height?: string;
@@ -12,9 +13,10 @@ export interface SectionProps {
 
 
 
-export default function Section({children, bgColor, height}: SectionProps) {
+export default function Section({id, children, bgColor, height}: SectionProps) {
     return (
         <section
+            id={id}
             className={`${bgColor}`}
             style={{height: height}}
         >
