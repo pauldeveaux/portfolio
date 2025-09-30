551cf0156a8a83748d1b877f2bb78a6522bb9c11
Paul Deveaux <paul.deveaux1@gmail.com>
2025-09-30 18:26:38 +0200

    style(ui): change title margin on TextSection

--- frontend/src/components/features/home_page/TextSection.tsx
+++ frontend/src/components/features/home_page/TextSection.tsx
@@ -9,7 +9,7 @@ interface TextSectionProps extends SectionProps {
 export default function TextSection( {title, text, ...sectionProps}: TextSectionProps) {
     return (
         <Section {...sectionProps}>
-            <div className="h-full flex flex-col items-center justify-start text-center gap-15 p-10 sm:p-20 lg:p-40">
+            <div className="h-full flex flex-col items-center justify-start text-center gap-15 p-10 sm:p-20">
                 <h2 className="text-5xl font-bold mb-4">{title}</h2>
                 <div className="text-lg whitespace-pre-line">
                     {text}
