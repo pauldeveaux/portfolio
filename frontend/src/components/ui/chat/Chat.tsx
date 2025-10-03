9358ecdbf16c87998cbc04781d8005b2844655c4
Paul Deveaux <paul.deveaux1@gmail.com>
2025-10-03 15:12:57 +0200

    feat(ui): AI pending message, and automatic response

--- frontend/src/components/ui/chat/Chat.tsx
+++ frontend/src/components/ui/chat/Chat.tsx
@@ -1,11 +1,11 @@
-import Message, {ChatMessageProps} from "@/components/ui/chat/Message";
+import Message, {ChatMessageProps, PENDING_MESSAGE} from "@/components/ui/chat/Message";
 import React, {useEffect, useRef, useState} from "react";
 import TextareaAutosize from 'react-textarea-autosize';
-import { Send } from "lucide-react"
+import {Send} from "lucide-react"
 import {useIsMobile} from "@/utils/useIsMobile";
 
 
-const AIMessage : ChatMessageProps = {
+const AIMessage: ChatMessageProps = {
     type: "ai",
     text: "Bonjour ! Je suis l’assistant virtuel de mon portfolio. Posez-moi des questions sur mes projets, compétences ou expériences.",
 }
@@ -15,13 +15,17 @@ export default function Chat() {
     const textareaRef = useRef<HTMLTextAreaElement>(null);
     const chatContainerRef = useRef<HTMLDivElement>(null);
     const [messages, setMessages] = useState<ChatMessageProps[]>([AIMessage]);
+    const [waiting, setWaiting] = useState(false); // bloque l’envoi pendant pending
     const isMobile = useIsMobile();
+
+    const timeoutsRef = useRef<number[]>([]);
+
 
     // Auto scroll
     const scrollToBottom = () => {
         if (!chatContainerRef.current) return;
         const container = chatContainerRef.current;
-        container.scrollTo({ top: container.scrollHeight, behavior: "smooth" });
+        container.scrollTo({top: container.scrollHeight, behavior: "smooth"});
     };
 
     useEffect(() => {
@@ -29,20 +33,56 @@ export default function Chat() {
     }, [messages]);
 
 
+    const waitingResponse = () => {
+        const typingDelayBeforeShowing = 500;
+        const timeoutId = window.setTimeout(() => {
+            setMessages((prev) => {
+                return [...prev, PENDING_MESSAGE];
+            });
+        }, typingDelayBeforeShowing);
+
+        timeoutsRef.current.push(timeoutId);
+    };
+
+    // Simule une réponse et remplace le dernier pending
+    const simulateResponse = (pendingIndex: number) => {
+        setTimeout(() => {
+            setMessages((prev) => {
+                const newMessages = [...prev];
+                if (newMessages[pendingIndex]?.type === "pending") {
+                    newMessages[pendingIndex] = {
+                        type: "ai",
+                        text: "Réponse automatique de l'IA",
+                    };
+                }
+                return newMessages;
+            });
+            setWaiting(false);
+        }, 2000);
+    };
+
 
     // Send Message
     const handleSend = () => {
+        if (waiting) return;
         if (!textareaRef.current) return;
         const text = textareaRef.current.value.trim();
         if (!text) return;
 
-        const newMessage: ChatMessageProps = {
-            type: "user",
-            text,
-        };
-
-        setMessages([...messages, newMessage]);
+        const userMessage: ChatMessageProps = {type: "user", text};
         textareaRef.current.value = "";
+
+        setMessages((prev) => {
+            const next = [...prev, userMessage, PENDING_MESSAGE];
+            const pendingIndex = next.length - 1;
+
+            setWaiting(true);
+
+            // TODO DELETE
+            simulateResponse(pendingIndex);
+
+            return next;
+        });
     };
 
     // Send via enter
@@ -72,17 +112,22 @@ export default function Chat() {
             </div>
 
             <div className="flex flex-row sm:flex-row gap-2 mt-2 items-end">
-                  < TextareaAutosize
-                      ref={textareaRef}
-                      placeholder="Posez une question ici..."
-                      className="flex-1 w-full p-3 rounded-2xl border border-gray-300 bg-white/50 backdrop-blur-sm resize-none focus:outline-none focus:ring-2 focus:ring-[#14A3A2]"
-                      maxRows={5}
-                      minRows={1}
-                      onKeyDown={handleKeyDown}
-                  />
+                < TextareaAutosize
+                    ref={textareaRef}
+                    placeholder="Posez une question ici..."
+                    className="flex-1 w-full p-3 rounded-2xl border border-gray-300 bg-white/50 backdrop-blur-sm resize-none focus:outline-none focus:ring-2 focus:ring-[#14A3A2]"
+                    maxRows={5}
+                    minRows={1}
+                    onKeyDown={handleKeyDown}
+                />
                 <button
-                    className="flex-shrink-0 px-5 py-3 rounded-2xl bg-[#107E7D] text-white font-semibold hover:bg-[#0E6B6B] hover:cursor-pointer"
+                    className={`flex-shrink-0 px-5 py-3 rounded-2xl font-semibold transition-colors
+                        ${waiting
+                            ? "bg-gray-400 text-gray-200 cursor-not-allowed"
+                            : "bg-[#107E7D] text-white hover:bg-[#0E6B6B] cursor-pointer"
+                        }`}
                     onClick={handleSend}
+                    disabled={waiting}
                 >
                     {isMobile ? <Send/> : <>Envoyer</>}
                 </button>
--- frontend/src/components/ui/chat/Message.tsx
+++ frontend/src/components/ui/chat/Message.tsx
@@ -1,26 +1,57 @@
-import { motion } from "motion/react";
-
+import {motion} from "motion/react";
 
 
 export interface ChatMessageProps {
     text: string;
-    type: "user" | "ai";
+    type: "user" | "ai" | "pending";
 }
 
-export default function Message({ text, type}: ChatMessageProps) {
+export const PENDING_MESSAGE : ChatMessageProps = {
+    type: "pending",
+    text: ""
+}
+
+const messageStyles: Record<ChatMessageProps["type"], string> = {
+    user: "self-end bg-gradient-to-r from-[#14A3A2] to-[#14A3A2] text-white drop-shadow-md",
+    ai: "self-start bg-gradient-to-r from-[#d9d9d9] to-[#f0f0f0] text-gray-900 drop-shadow-md",
+    pending: "self-start bg-gradient-to-r from-[#e0e0e0] to-[#f5f5f5] text-gray-500 italic drop-shadow-md",
+};
+
+export default function Message({text, type}: ChatMessageProps) {
+
     return (
-    <motion.div
-      initial={{ opacity: 0, y: 10 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.3 }}
-      className={`mb-3 p-4 max-w-[75%] rounded-xl shadow-md break-words
-        ${type === "user"
-          ? "self-end bg-gradient-to-r from-[#14A3A2] to-[#14A3A2] text-white drop-shadow-md"
-          : "self-start bg-gradient-to-r from-[#d9d9d9] to-[#f0f0f0] text-gray-900 drop-shadow-md"
-        }`}
-      style= {{ whiteSpace: "pre-wrap"}} // line breaks
-    >
-      {text}
+        <motion.div
+            initial={{opacity: 0, y: 10}}
+            animate={{opacity: 1, y: 0}}
+            transition={{duration: 0.3}}
+            className={`mb-3 p-4 max-w-[75%] rounded-xl shadow-md break-words ${messageStyles[type]}`}
+            style={{whiteSpace: "pre-wrap"}} // line breaks
+        >
+            {type === "pending" ? (
+        <div className="flex gap-1 w-6 h-4 align-center">
+          {/* Bulle fixe, 3 points animés */}
+          <motion.span
+            animate={{ opacity: [0.2, 1, 0.2] }}
+            transition={{ duration: 1, repeat: Infinity }}
+          >
+            •
+          </motion.span>
+          <motion.span
+            animate={{ opacity: [0.2, 1, 0.2] }}
+            transition={{ duration: 1, repeat: Infinity, delay: 0.2 }}
+          >
+            •
+          </motion.span>
+          <motion.span
+            animate={{ opacity: [0.2, 1, 0.2] }}
+            transition={{ duration: 1, repeat: Infinity, delay: 0.4 }}
+          >
+            •
+          </motion.span>
+        </div>
+      ) : (
+        text
+      )}
     </motion.div>
     )
 }
