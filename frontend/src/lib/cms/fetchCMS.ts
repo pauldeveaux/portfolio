6d8a4980f3d6be4ba847190a31cba29c14c6db50
Paul Deveaux <paul.deveaux1@gmail.com>
2025-10-06 10:40:14 +0200

    fix(cms): return single element in getHero

--- frontend/src/lib/cms/fetchCMS.ts
+++ frontend/src/lib/cms/fetchCMS.ts
@@ -1,18 +1,23 @@
-
 export async function fetchCMS<T>(endpoint: string, token?: string): Promise<T[]> {
-  const res = await fetch(`${process.env.CMS_API_URL}${endpoint}`, {
-    headers: token ? { Authorization: `Bearer ${token}` } : undefined,
-    cache: "no-store",
-  });
+    const res = await fetch(`${process.env.CMS_API_URL}${endpoint}`, {
+        headers: token ? {Authorization: `Bearer ${token}`} : undefined,
+        cache: "no-store",
+    });
 
-  if (!res.ok) throw new Error(`Failed to fetch ${endpoint}: ${res.statusText}`);
+    if (!res.ok) throw new Error(`Failed to fetch ${endpoint}: ${res.statusText}`);
 
-  const json = await res.json();
+    const json = await res.json();
 
-  return json.data as T[];
+    if (Array.isArray(json.data)) {
+        return json.data as T[];
+    } else if (json.data) {
+        return [json.data as T];
+    } else {
+        return [];
+    }
 }
 
 
-export function getImgFullUrl(imgUrl: string){
-  return `${process.env.CMS_API_URL}${imgUrl}`
+export function getImgFullUrl(imgUrl: string) {
+    return `${process.env.CMS_API_URL}${imgUrl}`
 }
--- frontend/src/lib/cms/hero.ts
+++ frontend/src/lib/cms/hero.ts
@@ -1,6 +1,12 @@
 import { Hero } from "@/types/cms";
 import { fetchCMS } from "./fetchCMS";
 
-export async function getHero(): Promise<Hero[]> {
-  return fetchCMS<Hero>("/hero", process.env.CMS_API_KEY);
+export async function getHero(): Promise<Hero> {
+  const heroes = await fetchCMS<Hero>("/hero", process.env.CMS_API_KEY);
+
+  if (!heroes || heroes.length === 0) {
+    throw new Error("No hero found");
+  }
+
+  return heroes[0];
 }
