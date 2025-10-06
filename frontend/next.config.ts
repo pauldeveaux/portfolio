6d59ce25edf4bcd2986ea90887e4e96fb4d3b3a1
Paul Deveaux <paul.deveaux1@gmail.com>
2025-10-06 10:33:48 +0200

    chore: Change gitignore & nextconfig

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,14 +1,22 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
+
+require('dotenv').config()
 
 const nextConfig: NextConfig = {
     images: {
         remotePatterns: [
             {
+                protocol: 'http',
+                hostname: 'localhost',
+                port: '1337',
+                pathname: '/api/uploads/**',
+            },
+            {
                 protocol: 'https',
                 hostname: 'cdn-icons-png.flaticon.com',
                 port: '',
                 pathname: '/512/**',
-            }
+            },
         ],
     },
 };
