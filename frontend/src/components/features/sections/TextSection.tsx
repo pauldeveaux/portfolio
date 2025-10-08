86a654cb038b83371ea25663ad4448912aa852e2
Paul Deveaux <paul.deveaux1@gmail.com>
2025-10-08 15:08:32 +0200

    feat(cms): add markdown to TextSection & reduce margin between text and title

--- frontend/src/components/features/sections/TextSection.tsx
+++ frontend/src/components/features/sections/TextSection.tsx
@@ -1,4 +1,8 @@
+"use client";
+
 import Section, { SectionProps } from "@/components/ui/layout/Section";
+import useMarkdownLoader from "@/components/features/markdown/markdownLoader";
+import React, {useEffect} from "react";
 
 /**
  * Props for the TextSection component.
@@ -18,17 +22,27 @@ interface TextSectionProps extends SectionProps {
  * Renders a titled section with a block of text.
  */
 export default function TextSection({ title, text, ...sectionProps }: TextSectionProps) {
+    const { content, loading, loadMarkdown } = useMarkdownLoader({});
+
+    useEffect(() => {
+        async function fetchMarkdown() {
+            await loadMarkdown({markdown: text});
+        }
+        fetchMarkdown();
+    }, [text]);
+
+
     return (
         <Section {...sectionProps}>
             {/* Container with vertical layout and spacing */}
-            <div className="h-full flex flex-col items-center justify-start text-center gap-15 p-10 sm:p-20">
+            <div className="h-full flex flex-col items-center justify-start text-center gap-8 p-10 sm:p-20">
 
                 {/* Section title */}
                 <h2 className="text-5xl font-bold mb-4">{title}</h2>
 
                 {/* Text content */}
-                <div className="text-lg whitespace-pre-line">
-                    {text}
+                <div className="text-lg whitespace-pre-line sm:max-w-7xl">
+                    {loading ? text : content}
                 </div>
             </div>
         </Section>
