3763876ef061b8c206c6d8c21d0680488edbe515
Paul Deveaux <paul.deveaux1@gmail.com>
2025-10-09 11:48:06 +0200

    feat(cms): markdown on portfolio card

--- frontend/src/components/ui/carousel/CarouselCard.tsx
+++ frontend/src/components/ui/carousel/CarouselCard.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { motion } from "framer-motion";
+import {motion} from "framer-motion";
+import useMarkdownLoader from "@/components/features/markdown/markdownLoader";
+import React, {useEffect} from "react";
 
 /** Props for a single CarouselCard */
 export interface CarouselCardProps {
@@ -18,16 +20,28 @@ export interface CarouselCardProps {
  * @param name - Card title
  * @param description - Optional descriptive text
  */
-export default function CarouselCard({ name, description }: CarouselCardProps) {
+export default function CarouselCard({name, description}: CarouselCardProps) {
+    const {content, loading, loadMarkdown} = useMarkdownLoader({});
+
+    useEffect(() => {
+        async function fetchMarkdown() {
+            await loadMarkdown({markdown: description});
+        }
+
+        fetchMarkdown();
+    }, [description, loadMarkdown]);
+
     return (
         <motion.div
             className="bg-white h-full flex flex-col flex-shrink-0 items-center text-center w-64 rounded-xl shadow-lg text-xl font-semibold"
-            whileHover={{ scale: 1.1 }}
-            transition={{ type: "spring", stiffness: 300 }}
+            whileHover={{scale: 1.1}}
+            transition={{type: "spring", stiffness: 300}}
         >
             <h3 className="font-semibold text-xl m-4 text-black">{name}</h3>
             {description && (
-                <p className="text-sm text-gray-500 mx-5 my-3 overflow-auto">{description}</p>
+                <div className="text-sm text-gray-500 mx-5 my-3 overflow-auto">
+                    {loading ? <p>text</p> : content}
+                </div>
             )}
         </motion.div>
     );
