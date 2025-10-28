3fa7d08d2dc55d1b2fbc0e84dd0fa5c29a490a16
Paul Deveaux <paul.deveaux1@gmail.com>
2025-10-28 12:34:20 +0100

    chore: Add google analytics

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,36 +1,57 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type {Metadata} from "next";
+import Script from 'next/script';
+import {Geist, Geist_Mono} from "next/font/google";
 import "../components/style/globals.css";
 import "react-vertical-timeline-component/style.min.css";
 
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Portfolio - Paul Deveaux",
-  description: "Bienvenue sur le portfolio de Paul Deveaux, ingénieur en intelligence artificielle.",
+    title: "Portfolio - Paul Deveaux",
+    description: "Bienvenue sur le portfolio de Paul Deveaux, ingénieur en intelligence artificielle.",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="fr">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    return (
+        <html lang="fr">
+        <head>
+            {/* Google Tag Manager */}
+            <Script
+                id="gtm-script"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PPF7XDZN');`,
+                }}
+            />
+        </head>
+        <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+        <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PPF7XDZN"
+                    height="0" width="0" style={{display: 'none', visibility: 'hidden'}}
+            >
+            </iframe>
+        </noscript>
         {children}
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
