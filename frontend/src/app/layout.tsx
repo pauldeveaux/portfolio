2a69f20484c7fdabad91b569e83933d275e8c310
Paul Deveaux <paul.deveaux1@gmail.com>
2025-11-04 11:48:08 +0100

    style(ui): Change fonts

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type {Metadata} from "next";
-import {Geist, Geist_Mono} from "next/font/google";
+import {Geist, Geist_Mono, Comic_Neue, Inter} from "next/font/google";
 import "../components/style/globals.css";
 import "react-vertical-timeline-component/style.min.css";
-import { GoogleTagManager } from "@next/third-parties/google";
+import {GoogleTagManager} from "@next/third-parties/google";
 
 
 const geistSans = Geist({
@@ -15,21 +15,34 @@ const geistMono = Geist_Mono({
     subsets: ["latin"],
 });
 
+
+const comicNeue = Comic_Neue({
+    variable: "--font-comic-neue",
+    subsets: ["latin"],
+    weight: ["400", "700"],
+})
+
+const inter = Inter({
+    variable: "--font-inter",
+    subsets: ["latin"],
+    weight: ["400", "600", "700"],
+})
+
 export const metadata: Metadata = {
-  title: "Portfolio - Paul Deveaux",
-  description: "Portfolio de Paul Deveaux, ingénieur en intelligence artificielle, présentant ses projets et réalisations.",
-  alternates: {
-    canonical: "https://paul-deveaux.fr",
-  },
-  openGraph: {
     title: "Portfolio - Paul Deveaux",
     description: "Portfolio de Paul Deveaux, ingénieur en intelligence artificielle, présentant ses projets et réalisations.",
-    url: "https://paul-deveaux.fr",
-    siteName: "Paul Deveaux",
-    images: [{ url: "https://paul-deveaux.fr/og-image.png", width: 1200, height: 630 }],
-    locale: "fr_FR",
-    type: "website",
-  },
+    alternates: {
+        canonical: "https://paul-deveaux.fr",
+    },
+    openGraph: {
+        title: "Portfolio - Paul Deveaux",
+        description: "Portfolio de Paul Deveaux, ingénieur en intelligence artificielle, présentant ses projets et réalisations.",
+        url: "https://paul-deveaux.fr",
+        siteName: "Paul Deveaux",
+        images: [{url: "https://paul-deveaux.fr/og-image.png", width: 1200, height: 630}],
+        locale: "fr_FR",
+        type: "website",
+    },
 };
 
 export default function RootLayout({
@@ -38,13 +51,15 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="fr">
-            <GoogleTagManager gtmId="GTM-PPF7XDZN"/>
-            <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-            >
-            {children}
-            </body>
+        <html
+            lang="fr"
+            className={`${geistSans.variable} ${geistMono.variable} ${comicNeue.variable} ${inter.variable} antialiased`}
+        >
+        <GoogleTagManager gtmId="GTM-PPF7XDZN"/>
+        <body
+        >
+        {children}
+        </body>
         </html>
     );
 }
